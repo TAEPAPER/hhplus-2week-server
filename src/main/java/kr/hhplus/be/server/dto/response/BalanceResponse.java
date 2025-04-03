3158e68a656e2993a02e9b5bb373f99710539ed8
TAEPAPER <taejeong055@gmail.com>
2025-04-03 19:59:49 +0900

    #12 Swagger: 잔액 충전/조회 API 작성

---
+++ src/main/java/kr/hhplus/be/server/dto/response/BalanceResponse.java
@@ -0,0 +1,15 @@
+package kr.hhplus.be.server.dto.response;
+
+import java.math.BigDecimal;
+
+// BalanceResponse
+public class BalanceResponse {
+    private Long userId;
+    private BigDecimal balance;
+
+    public BalanceResponse(Long userId, BigDecimal balance) {
+        this.userId  = userId;
+        this.balance = balance;
+    }
+    // constructor
+}
